a0f0ead09ca3177b6b682a33bcba2c51d791b7e3
Mig <mig@campbellhay.com>
2018-10-09 09:46:12 +0100

    Refactor

--- main.go
+++ main.go
@@ -4,98 +4,132 @@ import (
   "os"
   "fmt"
   "flag"
+  "errors"
   "github.com/mig-hub/nesrom/nesrom"
 )
 
 // Flags
+// Each set corresponds to a subcommand
 
-var checkCommand = flag.NewFlagSet("checkCommand", flag.ContinueOnError)
-var headerCommand = flag.NewFlagSet("headerCommand", flag.ContinueOnError)
+var checkCommand = flag.NewFlagSet("check", flag.ContinueOnError)
+var headerCommand = flag.NewFlagSet("header", flag.ContinueOnError)
 var hexOption = headerCommand.Bool("x", false, "display header as hexadecimal")
-var tilesCommand = flag.NewFlagSet("tilesCommand", flag.ContinueOnError)
+var tilesCommand = flag.NewFlagSet("tiles", flag.ContinueOnError)
 var colorOption = tilesCommand.Bool("c", false, "display tiles in color")
+var flagsets = []*flag.FlagSet{checkCommand,headerCommand,tilesCommand}
+
+// Main wraps another function which returns an int
+// This is so that it can exit and still execute defered calls
 
 func main() {
+  os.Exit(mainExitCode())
+}
+
+// The C-like main function which returns an int
+
+func mainExitCode() int {
 
   // Make sure there is a command
 
   if len(os.Args) < 2 {
-    fmt.Println("Error: command missing")
-    os.Exit(2)
+    fmt.Fprintln(os.Stderr, "Error: command missing")
+    return 1
+  }
+
+  // Set flag, file and header struct
+
+  fs, err := setFlagSet() 
+  if err != nil {
+    printErr(err)
+    return 1
+  }
+
+  f, err := setFile(fs)
+  if err != nil {
+    printErr(err)
+    return 1
+  }
+  defer f.Close()
+
+  h, err := setHeader(f)
+  if err != nil {
+    printErr(err)
+    return 1
   }
 
   // Handle command
 
-  var (
-    f *os.File
-    h *nesrom.Header
-  )
-
-  switch os.Args[1] {
+  switch fs.Name() {
   case "check":
-    f, h = initFileAndHeader(checkCommand)
+    // Fails before when invalid
     fmt.Println("Valid ROM")
   case "header":
-    f, h = initFileAndHeader(headerCommand)
     if *hexOption {
       printHexHeader(h)
     } else {
       printHeader(h)
     }
   case "tiles":
-    f, h = initFileAndHeader(tilesCommand)
     f.Seek(h.CHROffset(), 0)
     printTiles(f)
-  default:
-    fmt.Println("Error: command unknown", os.Args[1])
-    f.Close()
-    os.Exit(2)
   }
 
-  if f != nil {
-    f.Close()
+  return 0
+}
+
+func setFlagSet() (*flag.FlagSet, error) {
+
+  var fs *flag.FlagSet
+
+  // Find the flag with the subcommand name
+  for _, current_fs := range flagsets {
+    if current_fs.Name() == os.Args[1] {
+      fs = current_fs
+      break
+    }
+  }
+
+  if fs == nil {
+    return nil, errors.New(fmt.Sprint("Error: command unknown ", os.Args[1]))
+  } else {
+    fs.Parse(os.Args[2:])
+    return fs, nil
   }
 
 }
 
-func initFileAndHeader(fs *flag.FlagSet) (*os.File, *nesrom.Header) {
-
-  fs.Parse(os.Args[2:])
+func setFile(fs *flag.FlagSet) (*os.File, error) {
 
   // Make sure there is a filename and only one
 
   if len(fs.Args()) != 1 {
-    fmt.Println("Error: one ROM file should be passed as argument")
-    os.Exit(2)
+    return nil, errors.New("Error: one ROM file should be passed as argument")
   }
 
   // Open ROM file
 
   f, err := os.Open(fs.Arg(0))
   if err != nil {
-    fmt.Println("Error: cannot open ROM file", fs.Arg(0))
-    os.Exit(2)
+    return nil, errors.New(fmt.Sprint("Error: cannot open ROM file ", fs.Arg(0)))
   }
 
-  // ROM Header
+  return f, nil
+}
+
+func setHeader(f *os.File) (*nesrom.Header, error) {
 
   rawHeader := make([]byte, 16)
-  _, err = f.Read(rawHeader)
+  _, err := f.Read(rawHeader)
   if err != nil {
-    fmt.Println("Error: cannot read ROM file header")
-    f.Close()
-    os.Exit(2)
+    return nil, errors.New("Error: cannot read ROM file header")
   }
 
   h := nesrom.NewHeader(rawHeader)
   if ! h.IsValid() {
-    fmt.Println("Invalid ROM, magic signature does not match [ 4e 45 53 1a ]")
-    f.Close()
-    os.Exit(2)
+    return nil, errors.New("Invalid ROM, magic signature does not match [ 4e 45 53 1a ]")
   }
 
-  return f, h
-
+  return h, nil
 }
 
 func printHexHeader(h *nesrom.Header) {
@@ -118,13 +152,7 @@ func printHeader(h *nesrom.Header) {
   fmt.Println("Mapper:            ", h.MapperName())
 }
 
-func yesNo(b bool) string {
-  if b {
-    return "yes"
-  } else {
-    return "no"
-  }
-}
+// Chars used to represent pixels in the terminal
 
 // var drawPixelUnicode = []string{"  ", "\u2591\u2591", "\u2593\u2593", "\u2587\u2587" }
 var drawPixel = []string{"  ", "--", "//", "##" }
@@ -152,6 +180,20 @@ func printTiles(f *os.File) {
       fmt.Printf("\n")
     }
     fmt.Println("")
+  }
+}
+
+// Helpers
+
+func printErr(s error) {
+  fmt.Fprintln(os.Stderr, s)
+}
+
+func yesNo(b bool) string {
+  if b {
+    return "yes"
+  } else {
+    return "no"
   }
 }
 
